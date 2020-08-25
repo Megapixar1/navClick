88c887795a4b28fb522a730bfbd3d33fcffa683f
Megapixar1 <mopedo777@gmail.com>
2020-08-25 17:33:49 +0300

    navigation from click

---
+++ navClick.js
@@ -0,0 +1,18 @@
+
+jQuery(function($){
+$('a[href*="#"]').on('click.smoothscroll', function( e ){
+var hash    = this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
+if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
+var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
+if( ! $target.length ) return;
+e.preventDefault();
+$('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 400, 'swing', function(){
+window.location.hash = hash;
+});
+});
+});
+
+
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+// скирт для плавной навигации 
